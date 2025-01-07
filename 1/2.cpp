540b9058101a497abfed84595afaa0879be668ba
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-01-07 09:39:13 +0530

    Add files via upload

---
+++ 1/2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int Money_mom;
+    cout<<"Enter Money Given by Mom=";
+    cin>>Money_mom;
+
+    int Money_dad;
+    cout<<"Enter Money Given by dad=";
+    cin>>Money_dad;
+
+    int sum=Money_dad+Money_mom;
+    cout<<"Total="<<sum<<endl;
+}
+
+/*
+Enter Money Given by Mom=500
+Enter Money Given by dad=300
+Total=800
+*/
