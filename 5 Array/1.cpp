0878964ef6458d9f47f1f26602ac452161c5e527
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-01-16 09:32:10 +0530

    Add files via upload

---
+++ 5 Array/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int arr[5]={1,2,3,4,5};
+
+    for(i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+/*
+1 2 3 4 5
+1 2 3 4 5
+*/
