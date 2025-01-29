86adb5e61acb4e94e8710380f3a3a052339caf63
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-01-29 09:59:20 +0530

    Add files via upload

---
+++ 6 Char Arr/2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char arr[10]="Apple";
+    int i=0;
+
+    while(arr[i]!='\0'){
+        cout<<arr[i]<<endl;
+        i++;
+    }
+}
+
+/*
+A
+p
+p
+l
+e
+*/
