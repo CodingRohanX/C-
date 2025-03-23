340995368e3cb654ed5a8a357a8d90fcd4811de4
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-03-23 22:57:50 +0530

    Add files via upload

---
+++ 9 String/9 Length.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s1="Hi My name is Rohan";
+    for(int i=0;i<s1.length();i++){             //Length & Size are same
+        cout<<s1[i]<<endl;
+    }
+}
+
+/*
+H
+i
+
+M
+y
+
+n
+a
+m
+e
+
+i
+s
+
+R
+o
+h
+a
+n
+*/
