3c8f63bd1bbe563309086782b9c6a45d3ba454d6
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-06-08 21:33:00 +0530

    Add files via upload

---
+++ 15 Pair/1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+//Pair helps us to store diiferent types of value in a single structure.
+
+int main(){
+    pair<int,char> p;
+    p.first = 3;
+    p.second = 'f';
+}
