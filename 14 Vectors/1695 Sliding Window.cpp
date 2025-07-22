4ec7c5b08bcc73338ee43d04806da9e2f37a75f4
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-07-23 00:43:38 +0530

    Add files via upload

---
+++ 14 Vectors/1695 Sliding Window.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximumUniqueSubarray(vector<int>& n) {
+    if(n.size()==1) return n[0];
+    int p1=0,p2=0,sum=n[0],a=INT_MIN;
+    map<int,int> m;
+    m[n[0]]++;
+    while(p2<n.size()){
+        if(p2==n.size()-1) break;
+        if(m[n[p2+1]]==1){
+            p2++;
+            m[n[p2]]++;
+            sum+=n[p2];
+            while(m[n[p2]]>1){
+                m[n[p1]]--;
+                sum-=n[p1];
+                p1++;
+            }
+        }
+        else{
+            p2++;
+            m[n[p2]]++;
+            sum+=n[p2];
+        }
+        a=max(sum,a);
+    }
+    return a;
+}
