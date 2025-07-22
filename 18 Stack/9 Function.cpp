d0603ff113d6434040a152ec596bbdcc4f23cc18
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-07-23 00:46:06 +0530

    Add files via upload

---
+++ 18 Stack/9 Function.cpp
@@ -0,0 +1,19 @@
+#include <stack>
+
+using namespace std;
+
+stack<int> createStack() {
+    stack<int> myStack;
+    // You can push elements into the stack here if needed
+    return myStack;
+}
+
+int stackFunction(stack<int>& st){
+    
+}
+
+int main() {
+    stack<int> myStack = createStack();
+    // You can use the stack here as needed
+    return 0;
+}
