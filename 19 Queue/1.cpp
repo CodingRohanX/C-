a5209483a14a842f964111c0942211a3e2966930
Rohan Ravi <157512619+CodingRohanX@users.noreply.github.com>
2025-08-21 01:42:15 +0530

    Add files via upload

---
+++ 19 Queue/1.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+//#define n 20;
+int n=20;
+
+class queue{
+    int *arr;
+    int front;
+    int back;
+
+    public:
+    queue(){
+        arr=new int[n];
+        front=-1;
+        back=-1;
+    }
+
+    void push(int x){
+        if(back==n-1){
+            cout<<"Queue overflow"<<endl;
+            return;
+        }
+        back++;
+        arr[back]=x;
+
+        if(front==-1){      //only when adding 1st Element to the array.
+            front++;    
+        }
+    }
+
+    void pop(){
+        if(front==-1 || front>back){
+            cout<<"Queue is empty"<<endl;
+            return;
+        }
+        front++;
+    }
+
+    int peek(){
+        if(front==-1 || front>back){
+            cout<<"Queue is empty"<<endl;
+            return -1;
+        }
+        return arr[front];
+    }
+
+    bool empty(){
+        if(front==-1 || front>back){
+            return true;
+        }
+        return false;
+    }
+};
+
+int main(){
+    queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    cout<<q.peek()<<endl;       //1
+    q.pop();
+
+    cout<<q.peek()<<endl;       //2
+    q.pop();
+
+    cout<<q.peek()<<endl;       //3
+    q.pop();
+
+    cout<<q.peek()<<endl;       //4
+    q.pop();
+
+    cout<<q.empty()<<endl;      //1
+}
